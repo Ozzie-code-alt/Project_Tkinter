3223bcb1b99dd2473dfc50590defb109a61b3cae
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-03-30 12:18:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# GUI with Tkinter
+
+from tkinter import *  # basically import everything
+root = Tk()  #basically the start of every tkinter or main method
+
+myLabel = Label(root, text="Hello World") #label widget
+myLabel.pack() #Showing it to Screen
+
+root.mainloop()# basically loop infinitely aslong as the program['] runs
+
